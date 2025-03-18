f4ebcd0be78d6803a311e96d425ca3ef5d64462b
CoderDuck <hongdongk@naver.com>
2025-03-18 23:14:43 +0900

    Initial commit

---
+++ src/main/java/community/community_spring/CommunitySpringApplication.java
@@ -0,0 +1,13 @@
+package community.community_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/community/community_spring/CommunitySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package community.community_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
